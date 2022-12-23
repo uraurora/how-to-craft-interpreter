f0006a6120df9ec5ed937ae0e9e2305e189fff3c
uraurora <zzrzhezhiren@gmail.com>
2022-12-23 17:53:36 +0800

    add scanner

---
+++ jsora/src/main/java/com/uraurora/jsora/parser/Parser.java
@@ -0,0 +1,9 @@
+package main.java.com.uraurora.jsora.parser;
+
+public class Parser {
+    private String source;
+
+    public Parser(String source) {
+        this.source = source;
+    }
+}
---
+++ jsora/src/main/java/com/uraurora/jsora/scanner/Scanner.java
@@ -0,0 +1,18 @@
+package main.java.com.uraurora.jsora.scanner;
+
+import main.java.com.uraurora.jsora.common.Token;
+
+import java.util.List;
+
+public class Scanner {
+    private String source;
+
+    public Scanner(String source) {
+
+    }
+
+    public List<Token> scanTokens() {
+        // todo:
+        return null;
+    }
+}
