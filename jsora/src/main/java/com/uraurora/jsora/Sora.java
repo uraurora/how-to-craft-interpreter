5cb8d21c8514bb90dcf98122fa84fafc2325d26e
uraurora <zzrzhezhiren@gmail.com>
2022-12-23 17:50:20 +0800

    add scanner

---
+++ jsora/src/main/java/com/uraurora/jsora/Sora.java
@@ -0,0 +1,69 @@
+package main.java.com.uraurora.jsora;
+
+import main.java.com.uraurora.jsora.common.Token;
+import main.java.com.uraurora.jsora.scanner.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Sora {
+
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if(args == null || args.length > 1) {
+            System.out.println("Usage: sora [scripts]");
+            System.exit(64);
+        }else if (args.length == 1){
+            runFile(args[0]);
+        }else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(java.lang.String.valueOf(path)));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
