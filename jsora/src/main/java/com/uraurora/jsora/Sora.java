58e0160bba94fe617e38ef06ff75d1858c337ef9
uraurora <zzrzhezhiren@gmail.com>
2022-12-24 17:33:07 +0800

    add scanner code

--- jsora/src/main/java/com/uraurora/jsora/Sora.java
+++ jsora/src/main/java/com/uraurora/jsora/Sora.java
@@ -56,12 +56,11 @@ public class Sora {
         }
     }
 
-    static void error(int line, String message) {
+    public static void error(int line, String message) {
         report(line, "", message);
     }
 
-    private static void report(int line, String where,
-                               String message) {
+    private static void report(int line, String where, String message) {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
